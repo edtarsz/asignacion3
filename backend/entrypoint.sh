4bf1e977998731e801feb3771138da99626a7677
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-09-16 16:18:18 -0700

    Dockers terminados

---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# entrypoint.sh
+
+# Aplica cualquier migración pendiente de la base de datos
+echo "Aplicando migraciones de la base de datos..."
+npx prisma migrate deploy
+
+echo "Iniciando el servidor..."
+npm start
