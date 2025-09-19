aacbd2b91e829a52084dbcdf9aa5c007c41041dc
Roberto Garcia <jrobertogarcia16@gmail.com>
2025-09-18 21:34:36 -0700

    Switch database from PostgreSQL to MySQL and update related configurations

--- backend/src/config/sequelize.js
+++ backend/src/config/sequelize.js
@@ -2,11 +2,7 @@ import { Sequelize } from "sequelize";
 import "dotenv/config";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  protocol: "postgres",
-  dialectOptions: {
-    ssl: process.env.DB_SSL === "true" ? { require: true, rejectUnauthorized: false } : false,
-  },
+  dialect: "mysql",
   logging: false,
 });
 
